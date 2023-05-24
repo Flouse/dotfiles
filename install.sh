511bd358ce95e27880fe7df4ac585051886313cc
Flouse <1297478+Flouse@users.noreply.github.com>
2023-05-24 23:58:08 +0800

    Add install.sh -> install starship if it is not installed

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+function await {
+	$@ &
+	local pid=$!
+	wait $pid
+
+	if [ $? -ne 0 ]; then
+		echo "Failed to execute $@"
+		exit $?
+	fi
+}
+
+# Check if Starship is installed
+if ! command -v starship &> /dev/null; then
+  # Install Starship
+  echo "Installing starship"
+  await curl -fsSL https://starship.rs/install.sh | bash
+
+  # Add Starship initialization to ~/.bashrc
+  # echo 'eval "$(starship init bash)"' >> ~/.bashrc
+
+  # Reload shell configuration
+  # source ~/.bashrc
+else
+  echo "Starship is already installed."
+fi
