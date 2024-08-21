ad4f8c4e9c5588b9030cbe4ebbcd1f8f0999e2a4
Flouse <1297478+Flouse@users.noreply.github.com>
2024-08-21 12:50:15 +0000

    backup ~/.gitconfig and copy ./.gitconfig to ~/.gitconfig

--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
 # Git config
 echo "Setup git..."
-git config --global user.email "1297478+Flouse@users.noreply.github.com"
-git config --global user.name "Flouse"
+
+# backup ~/.gitconfig and copy ./.gitconfig to ~/.gitconfig
+if [ -f ~/.gitconfig ]; then
+  
+  mv ~/.gitconfig ~/.gitconfig.bak
+fi
+cp ./.gitconfig ~/.gitconfig
