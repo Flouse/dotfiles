5b236d2a9a80da7f95b8db7112db7d8abc6e9920
Flouse <1297478+Flouse@users.noreply.github.com>
2025-03-22 15:29:44 +0000

    refactor: only install rust if needed

--- setup.sh
+++ setup.sh
@@ -8,9 +8,11 @@ fi
 cp ./.gitconfig ~/.gitconfig
 
 
-# Add useful aliases to ~/.bash_aliases
 function add_aliases {
-  echo 'alias ll="ls -lah"' >> ~/.bash_aliases
+  echo "Add useful aliases to ~/.bash_aliases"
+
+  cat ./script/rustup >> ~/.bash_aliases
+
   echo 'alias gs="git status"' >> ~/.bash_aliases
   echo 'alias gp="git pull"' >> ~/.bash_aliases
   echo 'alias gc="git commit"' >> ~/.bash_aliases
