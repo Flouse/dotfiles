e9955fbdb61abe54c1d0e41f167cf421db797752
Flouse <1297478+Flouse@users.noreply.github.com>
2025-04-16 17:07:46 +0800

    Update alias in setup.sh

--- setup.sh
+++ setup.sh
@@ -13,11 +13,16 @@ function add_aliases {
 
   cat ./script/rustup >> ~/.bash_aliases
 
+  echo 'alias gl="git logs"' >> ~/.bash_aliases
   echo 'alias gs="git status"' >> ~/.bash_aliases
   echo 'alias gp="git pull"' >> ~/.bash_aliases
   echo 'alias gc="git commit"' >> ~/.bash_aliases
   echo 'alias ..="cd .."' >> ~/.bash_aliases
+
+  # Docker Alias
   echo 'alias compose="docker compose"' >> ~/.bash_aliases
+  # TODO: --user (id -u "$USER"):(id -g "$USER")
+  # echo "alias zx='docker run --interactive --tty --rm --volume "$PWD":/tmp --workdir /tmp --network=host zx'" >> ~/.bash_aliases
 }
 # Add useful aliases
 add_aliases
